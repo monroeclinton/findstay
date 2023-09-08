71c52f08b93bf1b8ca663a6b423ac5bde96bd72a
Monroe Clinton <contact@monroeclinton.com>
2023-09-07 21:10:56 -0400

    Parse search buffer

--- src/utils/gmm.ts
+++ src/utils/gmm.ts
@@ -49,6 +49,61 @@ const getLink = (
     return `https://www.google.com/maps/place/@${latitude},${longitude},15z/data=!4m6!3m5!1s${hex}!8m2!3d${latitude}!4d${longitude}!16s${uri}`;
 };
 
+export const parseBuffer = (data: unknown): ISearchData | null => {
+    const search = getArray(data, [0, 0]);
+    if (!search) return null;
+
+    const raw = getArray(data, [0, 1]);
+    if (!Array.isArray(raw)) return null;
+    raw.shift();
+
+    const results: Array<ISearchLocation> = [];
+    for (const store of raw) {
+        const name = getArray(store, [14, 11]);
+        const type = getArray(store, [14, 88, 1]);
+        const reviews = getArray(store, [14, 4, 8]);
+        const stars = getArray(store, [14, 4, 7]);
+        const latitude = getArray(store, [14, 9, 2]);
+        const longitude = getArray(store, [14, 9, 3]);
+        const hex = getArray(store, [14, 4, 10]);
+        const uri = getArray(store, [14, 4, 89]);
+
+        if (
+            name === null ||
+            type === null ||
+            reviews === null ||
+            stars === null ||
+            latitude === null ||
+            longitude === null ||
+            hex === null
+        ) {
+            continue;
+        }
+
+        results.push({
+            name: name as string,
+            type: type as string,
+            reviews: reviews as number,
+            stars: stars as number,
+            link: getLink(
+                latitude as number,
+                longitude as number,
+                hex as string,
+                uri as string
+            ),
+            coordinates: {
+                latitude: latitude as number,
+                longitutde: longitude as number,
+            },
+        });
+    }
+
+    return {
+        search: search as string,
+        results,
+    };
+};
+
 export const getInitialData = async (
     latitude: number,
     longitutde: number
