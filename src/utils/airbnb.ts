5b109a06ce31c2d3541ea8df9ec85de7c28da5d0
Monroe Clinton <contact@monroeclinton.com>
2023-09-13 21:59:21 -0400

    Create axios request for Airbnb API

--- src/utils/airbnb.ts
+++ src/utils/airbnb.ts
@@ -3,15 +3,32 @@ import axios, { type AxiosResponse } from "axios";
 const headers = {
     "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0",
-    "Content-Type":
-        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+};
+
+const scrapeAirbnbApi = async (apiKey: string) => {
+    const res: AxiosResponse<string> = await axios.post(
+        `https://www.airbnb.com/api/v3/StaysMapS2Search?operationName=StaysMapS2Search&locale=en&currency=USD`,
+        {
+            headers: {
+                ...headers,
+                "X-Airbnb-API-Key": apiKey,
+                "Content-Type": "application/json",
+            },
+        }
+    );
+
+    console.log(res.data);
 };
 
 export const syncAirbnbListings = async (location: string) => {
     const res: AxiosResponse<string> = await axios.get(
         `https://www.airbnb.com/s/${location}/homes`,
         {
-            headers,
+            headers: {
+                ...headers,
+                "Content-Type":
+                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            },
         }
     );
 
