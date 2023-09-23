13e74fcee401011b7fb8d2be26384d973df13465
Monroe Clinton <contact@monroeclinton.com>
2023-09-22 22:37:23 -0400

    Update Side to new packages

--- src/components/Side/index.tsx
+++ src/components/Side/index.tsx
@@ -1,8 +1,10 @@
-import { Center, createStyles, Navbar, Title } from "@mantine/core";
+import { AppShell, Center, Flex, Title } from "@mantine/core";
 import { IconAdjustments, IconHome } from "@tabler/icons-react";
 
 import UserButton from "~/components/Button/UserButton";
 import Link from "~/components/Side/Link";
+
+import classes from "./Side.module.css";
 
 interface Item {
     label: string;
@@ -15,44 +17,34 @@ const items: Item[] = [
     { label: "Settings", icon: IconAdjustments, link: "/settings" },
 ];
 
-const useStyles = createStyles((theme) => ({
-    navbar: {
-        background:
-            theme.colorScheme === "dark"
-                ? theme.colors.gray[1]
-                : theme.colors.gray[1],
-    },
-
-    header: {
-        padding: theme.spacing.md,
-        marginBottom: `calc(${theme.spacing.md} * 1.5)`,
-    },
-
-    footer: {
-        paddingTop: theme.spacing.md,
-        marginTop: theme.spacing.md,
-    },
-}));
-
 const Side = () => {
-    const { classes } = useStyles();
-
     const links = items.map((item) => <Link {...item} key={item.label} />);
 
     return (
-        <Navbar width={{ base: SIDE_WIDTH }} p="md" className={classes.navbar}>
-            <Navbar.Section className={classes.header}>
+        <AppShell.Navbar
+            w={{ base: SIDE_WIDTH }}
+            p="md"
+            className={classes.navbar}
+        >
+            <div className={classes.header}>
                 <Center>
                     <Title>FindBase</Title>
                 </Center>
-            </Navbar.Section>
+            </div>
 
-            <Navbar.Section grow>{links}</Navbar.Section>
+            <Flex
+                direction="column"
+                style={{
+                    flexGrow: 1,
+                }}
+            >
+                {links}
+            </Flex>
 
-            <Navbar.Section className={classes.footer}>
-                <UserButton />
-            </Navbar.Section>
-        </Navbar>
+            <div className={classes.footer}>
+                <UserButton w="100%" />
+            </div>
+        </AppShell.Navbar>
     );
 };
 
