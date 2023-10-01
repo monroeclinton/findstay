42682745e311df4336fe2718ab5d0fce105bf284
Monroe Clinton <contact@monroeclinton.com>
2023-09-30 23:13:04 -0400

    Show popup on map click

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,7 +1,9 @@
-import { Badge } from "@mantine/core";
+import { Badge, Card, Flex, Image, Text, ThemeIcon } from "@mantine/core";
+import { IconStarFilled } from "@tabler/icons-react";
 import type { inferRouterOutputs } from "@trpc/server";
 import { Point } from "ol/geom";
 import { fromLonLat } from "ol/proj";
+import { useState } from "react";
 import { RFeature, RLayerVector, RMap, ROSM, ROverlay } from "rlayers";
 
 import { type AppRouter } from "~/server/api/root";
@@ -13,6 +15,8 @@ interface IMapProps {
 }
 
 const Map = ({ data }: IMapProps) => {
+    const [selected, setSelected] = useState<null | string>(null);
+
     return (
         <RMap
             width="100%"
@@ -26,7 +30,7 @@ const Map = ({ data }: IMapProps) => {
             }}
         >
             <ROSM />
-            <RLayerVector zIndex={10}>
+            <RLayerVector zIndex={100}>
                 {data.locations.map((record) => (
                     <RFeature
                         key={record.id}
@@ -35,12 +39,83 @@ const Map = ({ data }: IMapProps) => {
                                 fromLonLat([record.longitude, record.latitude])
                             )
                         }
-                        onClick={(e) => {
-                            console.log("clicked");
-                        }}
                     >
-                        <ROverlay className="example-overlay">
-                            <Badge color="pink">${record.price}</Badge>
+                        <ROverlay
+                            positioning="top-center"
+                            offset={[0, 15]}
+                            key={record.id}
+                        >
+                            {selected === record.id && (
+                                <Card w={300} withBorder>
+                                    <Card.Section mb="md">
+                                        <Image
+                                            height={200}
+                                            width="100%"
+                                            src={record.images.at(0)}
+                                            alt="Airbnb image"
+                                        />
+                                    </Card.Section>
+
+                                    <Text fw={500} lineClamp={1}>
+                                        {record.name}
+                                    </Text>
+
+                                    <Flex justify="space-between">
+                                        <Flex align="center">
+                                            <ThemeIcon
+                                                size="xs"
+                                                color="yellow"
+                                                variant="light"
+                                            >
+                                                <IconStarFilled />
+                                            </ThemeIcon>
+                                            <Text
+                                                fw={600}
+                                                size="sm"
+                                                c="gray"
+                                                style={{
+                                                    marginLeft: "6px",
+                                                }}
+                                            >
+                                                {record.ratings
+                                                    .split("out of 5")
+                                                    .at(0)}
+                                            </Text>
+                                        </Flex>
+                                        <Text fw={600} c="green" mt="xs">
+                                            ${record.price} / night
+                                        </Text>
+                                    </Flex>
+                                </Card>
+                            )}
+                        </ROverlay>
+                    </RFeature>
+                ))}
+            </RLayerVector>
+            <RLayerVector>
+                {data.locations.map((record) => (
+                    <RFeature
+                        key={record.id}
+                        geometry={
+                            new Point(
+                                fromLonLat([record.longitude, record.latitude])
+                            )
+                        }
+                    >
+                        <ROverlay positioning="center-center">
+                            <Badge
+                                style={{
+                                    cursor: "pointer",
+                                }}
+                                color={
+                                    selected === record.id ? "black" : "pink"
+                                }
+                                onClick={() => {
+                                    setSelected(record.id);
+                                }}
+                            >
+                                ${record.price}
+                            </Badge>
                         </ROverlay>
                     </RFeature>
                 ))}
