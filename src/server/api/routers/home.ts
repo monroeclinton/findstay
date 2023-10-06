1a789e5389f3faacf017ee9fc0ece2fd4ddd7699
Monroe Clinton <contact@monroeclinton.com>
2023-10-06 19:37:43 -0400

    Return midpoint

--- src/server/api/routers/home.ts
+++ src/server/api/routers/home.ts
@@ -79,6 +79,7 @@ export const homeRouter = createTRPCRouter({
             return {
                 id: airbnbSync.id,
                 cursors: airbnbSync.cursors,
+                midpoint,
             };
         }),
     getPage: publicProcedure
