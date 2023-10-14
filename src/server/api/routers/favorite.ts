93dd2596da12c5b6f3b3d5caa4e4ab2d47d9423c
Monroe Clinton <contact@monroeclinton.com>
2023-10-14 18:08:23 -0300

    Return Airbnb locations for favorites

--- src/server/api/routers/favorite.ts
+++ src/server/api/routers/favorite.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import { addComputedFields } from "~/utils/home";
 
 export const favoriteRouter = createTRPCRouter({
     create: protectedProcedure
@@ -47,10 +48,20 @@ export const favoriteRouter = createTRPCRouter({
     getAll: protectedProcedure.query(async ({ ctx, input }) => {
         const session = ctx.session;
 
-        return ctx.prisma.airbnbLocationFavorite.findMany({
+        const favorites = await ctx.prisma.airbnbLocationFavorite.findMany({
             where: {
                 userId: session.user.id,
             }
+        })
+
+        const locations = await ctx.prisma.airbnbLocation.findMany({
+            where: {
+                id: {
+                    in: favorites.map(favorite => favorite.locationId),
+                },
+            }
         });
+
+        return addComputedFields(locations, session.user.id);
     })
 });
