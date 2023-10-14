5cb9182cfbd52d57fe4309d74b3ab385b9cd7143
Monroe Clinton <contact@monroeclinton.com>
2023-10-13 21:20:45 -0300

    Import geometry

--- src/server/api/routers/home.ts
+++ src/server/api/routers/home.ts
@@ -4,57 +4,7 @@ import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { createAirbnbSync, syncAirbnbPage } from "~/utils/airbnb";
 import { syncSuperMarkets } from "~/utils/gmm";
-
-// https://stackoverflow.com/q/18883601
-function getDistanceFromLatLonInKm(
-    lat1: number,
-    lon1: number,
-    lat2: number,
-    lon2: number
-) {
-    const R = 6371; // Radius of the earth in km
-    const dLat = deg2rad(lat2 - lat1); // deg2rad below
-    const dLon = deg2rad(lon2 - lon1);
-    const a =
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(deg2rad(lat1)) *
-            Math.cos(deg2rad(lat2)) *
-            Math.sin(dLon / 2) *
-            Math.sin(dLon / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const d = R * c; // Distance in km
-    return d;
-}
-
-// https://stackoverflow.com/a/4656937
-function getMidPoint(lat1: number, lon1: number, lat2: number, lon2: number) {
-    const dLon = deg2rad(lon2 - lon1);
-
-    lat1 = deg2rad(lat1);
-    lat2 = deg2rad(lat2);
-    lon1 = deg2rad(lon1);
-
-    const Bx = Math.cos(lat2) * Math.cos(dLon);
-    const By = Math.cos(lat2) * Math.sin(dLon);
-    const lat3 = Math.atan2(
-        Math.sin(lat1) + Math.sin(lat2),
-        Math.sqrt((Math.cos(lat1) + Bx) * (Math.cos(lat1) + Bx) + By * By)
-    );
-    const lon3 = lon1 + Math.atan2(By, Math.cos(lat1) + Bx);
-
-    return {
-        latitude: rad2deg(lat3),
-        longitude: rad2deg(lon3),
-    };
-}
-
-function rad2deg(rad: number) {
-    return rad * (180 / Math.PI);
-}
-
-function deg2rad(deg: number) {
-    return deg * (Math.PI / 180);
-}
+import { getMidPoint } from "~/utils/geometry";
 
 export const homeRouter = createTRPCRouter({
     createSync: protectedProcedure
