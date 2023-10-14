c5b850e98afed51fe8abd3011cec2c6bb210883d
Monroe Clinton <contact@monroeclinton.com>
2023-10-14 18:08:39 -0300

    Use addComputedFields for search

--- src/server/api/routers/home.ts
+++ src/server/api/routers/home.ts
@@ -4,7 +4,8 @@ import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { createAirbnbSync, syncAirbnbPage } from "~/utils/airbnb";
 import { syncSuperMarkets } from "~/utils/gmm";
-import { getMidPoint, getDistanceFromLatLonInKm } from "~/utils/geometry";
+import { addComputedFields } from "~/utils/home";
+import { getMidPoint } from "~/utils/geometry";
 
 export const homeRouter = createTRPCRouter({
     createSync: protectedProcedure
@@ -50,94 +51,13 @@ export const homeRouter = createTRPCRouter({
                 airbnbSync.swLongitude.toNumber()
             );
 
-            const supermarkets = await ctx.prisma.$queryRaw<
-                GoogleMapsLocation[]
-            >(
-                Prisma.sql`
-                    SELECT
-                        id,
-                        "syncId",
-                        name,
-                        type,
-                        reviews,
-                        stars,
-                        hex,
-                        uri,
-                        link,
-                        latitude,
-                        longitude,
-                        "updatedAt",
-                        "createdAt"
-                    FROM
-                        google_maps_location
-                    WHERE
-                        ST_DistanceSphere(
-                            coordinate,
-                            ST_MakePoint(${midpoint.latitude}, ${midpoint.longitude})
-                        ) <= 2000
-                `
-            );
-
-            const closestSupermarket = ({
-                latitude,
-                longitude,
-            }: {
-                latitude: number;
-                longitude: number;
-            }): number => {
-                let shortest = null;
-
-                for (const supermarket of supermarkets) {
-                    const distance = getDistanceFromLatLonInKm(
-                        supermarket.latitude.toNumber(),
-                        supermarket.longitude.toNumber(),
-                        latitude,
-                        longitude
-                    );
-
-                    if (shortest === null || shortest > distance) {
-                        shortest = distance;
-                    }
-                }
-
-                return Math.round((shortest || 0) * 1000);
-            };
-
             const page =
                 airbnbSync.pages.find((page) => page.cursor === input.cursor) ||
                 airbnbSync.pages.at(0);
 
             if (!page) throw new Error("Unable to fetch page of listings");
 
-            const favorites = await ctx.prisma.airbnbLocationFavorite.findMany({
-                where: {
-                    userId: ctx.session?.user.id,
-                },
-            });
-            const favoriteIds = favorites.map(
-                (favorite) => favorite.locationId
-            );
-
-            const locations = [];
-            for (const result of page.locations) {
-                const home = result.location;
-
-                locations.push({
-                    id: home.id,
-                    name: home.name,
-                    price: home.price,
-                    ratings: home.rating,
-                    longitude: home.longitude.toNumber(),
-                    latitude: home.latitude.toNumber(),
-                    supermarket: closestSupermarket({
-                        longitude: home.longitude.toNumber(),
-                        latitude: home.latitude.toNumber(),
-                    }),
-                    images: home.images,
-                    link: "https://airbnb.com/rooms/" + home.id,
-                    isFavorited: favoriteIds.includes(home.id),
-                });
-            }
+            const locations = await addComputedFields(page.locations.map(result => result.location), ctx.session.user.id);
 
             const cursorPos: number = input.cursor
                 ? airbnbSync.cursors.indexOf(input.cursor) || 0
