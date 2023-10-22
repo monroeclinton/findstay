2cd99a5639af4f0d644a3ee3593130914be6f118
Monroe Clinton <contact@monroeclinton.com>
2023-10-21 21:59:27 -0300

    zoomLevel func

--- src/utils/geometry.ts
+++ src/utils/geometry.ts
@@ -1,3 +1,30 @@
+// https://stackoverflow.com/a/13274361
+export const zoomLevel = (
+    neLat: number,
+    neLng: number,
+    swLat: number,
+    swLng: number,
+    map: { height: number, width: number }
+): number => {
+    console.log(neLat, neLng, swLat, swLng);
+    let WORLD_DIM = { height: 256, width: 256 };
+    let ZOOM_MAX = 21;
+
+    const zoom = (mapPx: number, worldPx: number, fraction: number): number => {
+        return Math.floor(Math.log(mapPx / worldPx / fraction) / Math.LN2);
+    }
+
+    let latFraction = (latRad(neLat) - latRad(swLat)) / Math.PI;
+
+    let lngDiff = neLng - swLng;
+    let lngFraction = ((lngDiff < 0) ? (lngDiff + 360) : lngDiff) / 360;
+
+    let latZoom = zoom(map.height, WORLD_DIM.height, latFraction);
+    let lngZoom = zoom(map.width, WORLD_DIM.width, lngFraction);
+
+    return Math.min(latZoom, lngZoom, ZOOM_MAX);
+}
+
 // https://stackoverflow.com/q/18883601
 export const getDistanceFromLatLonInKm = (
     lat1: number,
@@ -57,3 +84,8 @@ const deg2rad = (deg: number): number => {
     return deg * (Math.PI / 180);
 }
 
+const latRad = (lat: number): number => {
+    let sin = Math.sin(lat * Math.PI / 180);
+    let radX2 = Math.log((1 + sin) / (1 - sin)) / 2;
+    return Math.max(Math.min(radX2, Math.PI), -Math.PI) / 2;
+}
