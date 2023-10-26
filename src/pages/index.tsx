b5a5046b51282e04722ada80e83fa5b04b59c579
Monroe Clinton <contact@monroeclinton.com>
2023-10-25 22:29:42 -0300

    Enter email front page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,22 @@
+import { Button, Container, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { IconArrowRight } from "@tabler/icons-react";
 import Head from "next/head";
 
 import { type FindBasePage } from "~/types/next";
 
 const Home: FindBasePage = () => {
+    const form = useForm({
+        initialValues: {
+            email: "",
+        },
+
+        validate: {
+            email: (value) =>
+                /^\S+@\S+$/.test(value) ? null : "Invalid email",
+        },
+    });
+
     return (
         <>
             <Head>
@@ -10,6 +24,18 @@ const Home: FindBasePage = () => {
                 <meta name="description" content="Find the perfect stay." />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            <Container>
+                <form onSubmit={form.onSubmit((values) => console.log(values))}>
+                    <TextInput
+                        label="Your email"
+                        {...form.getInputProps("email")}
+                        onBlur={() => form.validate()}
+                    />
+                    <Button type="submit" rightSection={<IconArrowRight />}>
+                        Find your stay
+                    </Button>
+                </form>
+            </Container>
         </>
     );
 };
