6aa320e98de4802feb40bafb1d63ffa1691576cd
Monroe Clinton <contact@monroeclinton.com>
2023-10-27 20:58:25 -0300

    New invoice migration

---
+++ prisma/migrations/20231027151056_create_invoice/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Invoice" (
+    "id" TEXT NOT NULL,
+    "txId" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "paid" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Invoice_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Invoice_txId_key" ON "Invoice"("txId");
