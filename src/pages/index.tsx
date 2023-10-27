271e1a4ce44f6ed99b81ae18e78393450ece4a33
Monroe Clinton <contact@monroeclinton.com>
2023-10-26 22:20:00 -0300

    Create invoice

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
-import { Button, Container, TextInput } from "@mantine/core";
+import { Button, Container, Group, TextInput } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { IconArrowRight } from "@tabler/icons-react";
 import Head from "next/head";
 
 import { type FindBasePage } from "~/types/next";
+import { api } from "~/utils/api";
 
 const Home: FindBasePage = () => {
+    const createInvoice = api.invoice.create.useMutation({
+        onSuccess: (data) => {
+            window.location.href = data.url;
+        },
+    });
+
     const form = useForm({
         initialValues: {
             email: "",
@@ -25,15 +32,24 @@ const Home: FindBasePage = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Container>
-                <form onSubmit={form.onSubmit((values) => console.log(values))}>
+                <form
+                    onSubmit={form.onSubmit((values) =>
+                        createInvoice.mutate({
+                            email: values.email,
+                        })
+                    )}
+                >
                     <TextInput
                         label="Your email"
                         {...form.getInputProps("email")}
                         onBlur={() => form.validate()}
                     />
-                    <Button type="submit" rightSection={<IconArrowRight />}>
-                        Find your stay
-                    </Button>
+
+                    <Group mt="md">
+                        <Button type="submit" rightSection={<IconArrowRight />}>
+                            Find your stay
+                        </Button>
+                    </Group>
                 </form>
             </Container>
         </>
