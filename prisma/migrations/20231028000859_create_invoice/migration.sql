4b6a277124d296cfebfd5d6a7f2f762fd4846fe1
Monroe Clinton <contact@monroeclinton.com>
2023-10-27 22:29:09 -0300

    Update migration

--- prisma/migrations/20231028000859_create_invoice/migration.sql
+++ prisma/migrations/20231028000859_create_invoice/migration.sql
@@ -1,13 +1,13 @@
 -- CreateTable
-CREATE TABLE "Invoice" (
+CREATE TABLE "billing_invoice" (
     "id" TEXT NOT NULL,
     "txId" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "paid" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "Invoice_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "billing_invoice_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Invoice_txId_key" ON "Invoice"("txId");
+CREATE UNIQUE INDEX "billing_invoice_txId_key" ON "billing_invoice"("txId");
