fdbaecdb1639a58e5f0d0ce5714b4729ccf58fed
Monroe Clinton <contact@monroeclinton.com>
2023-11-08 22:08:32 -0300

    create useQueryParams hook

---
+++ src/hooks/useQueryParams.ts
@@ -0,0 +1,33 @@
+import {
+    type ReadonlyURLSearchParams,
+    usePathname,
+    useRouter,
+    useSearchParams,
+} from "next/navigation";
+
+export const useQueryParams = <T>(): [
+    ReadonlyURLSearchParams,
+    (_: Partial<T>) => void
+] => {
+    const router = useRouter();
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+    const urlSearchParams = new URLSearchParams(searchParams?.toString());
+
+    function setQueryParams(params: Partial<T>) {
+        Object.entries(params).forEach(([key, value]) => {
+            if (value === undefined || value === null) {
+                urlSearchParams.delete(key);
+            } else {
+                urlSearchParams.set(key, String(value));
+            }
+        });
+
+        const search = urlSearchParams.toString();
+        const query = search ? `?${search}` : "";
+        // replace since we don't want to build a history
+        router.replace(`${pathname}${query}`);
+    }
+
+    return [searchParams, setQueryParams];
+};
