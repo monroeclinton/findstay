e94037acb7b573fab05ecaab0cf6216d6b4fb559
Monroe Clinton <contact@monroeclinton.com>
2023-11-10 22:54:15 -0300

    useCallback for useQueryParams

--- src/hooks/useQueryParams.ts
+++ src/hooks/useQueryParams.ts
@@ -1,33 +1,39 @@
-import {
-    type ReadonlyURLSearchParams,
-    usePathname,
-    useRouter,
-    useSearchParams,
-} from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
 
 export const useQueryParams = <T>(): [
-    ReadonlyURLSearchParams,
+    (_: string) => string | null,
     (_: Partial<T>) => void
 ] => {
     const router = useRouter();
     const pathname = usePathname();
     const searchParams = useSearchParams();
-    const urlSearchParams = new URLSearchParams(searchParams?.toString());
 
-    function setQueryParams(params: Partial<T>) {
-        Object.entries(params).forEach(([key, value]) => {
-            if (value === undefined || value === null) {
-                urlSearchParams.delete(key);
-            } else {
-                urlSearchParams.set(key, String(value));
-            }
-        });
+    const getQueryParam = useCallback(
+        (key: string) => searchParams.get(key),
+        [searchParams]
+    );
 
-        const search = urlSearchParams.toString();
-        const query = search ? `?${search}` : "";
-        // replace since we don't want to build a history
-        router.replace(`${pathname}${query}`);
-    }
+    const setQueryParams = useCallback(
+        (params: Partial<T>) => {
+            const urlSearchParams = new URLSearchParams(
+                Object.fromEntries(searchParams)
+            );
 
-    return [searchParams, setQueryParams];
+            Object.entries(params).forEach(([key, value]) => {
+                if (value === undefined || value === null) {
+                    urlSearchParams.delete(key);
+                } else {
+                    urlSearchParams.set(key, String(value));
+                }
+            });
+
+            const search = urlSearchParams.toString();
+            const query = search ? `?${search}` : "";
+            router.replace(`${pathname}${query}`);
+        },
+        [pathname, router, searchParams]
+    );
+
+    return [getQueryParam, setQueryParams];
 };
