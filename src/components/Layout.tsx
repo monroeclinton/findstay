a690b270ef5ec187c7a42606fdf71ffe2f927900
Monroe Clinton <contact@monroeclinton.com>
2023-11-14 20:43:10 -0300

    AppShell mobile support

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,13 @@
-import { AppShell, type AppShellProps, Container, Flex } from "@mantine/core";
+import {
+    AppShell,
+    type AppShellProps,
+    Burger,
+    Container,
+    Flex,
+    Group,
+    Title,
+} from "@mantine/core";
+import { useDisclosure, useMediaQuery } from "@mantine/hooks";
 
 import Side, { SIDE_BREAKPOINT, SIDE_WIDTH } from "~/components/Side";
 
@@ -7,16 +16,25 @@ interface ILayoutProps extends AppShellProps {
 }
 
 const Layout = ({ children, container = true, ...props }: ILayoutProps) => {
+    const isMobile = useMediaQuery(`(max-width: ${SIDE_BREAKPOINT}px)`);
+    const [opened, { toggle }] = useDisclosure();
+
     const mainChildren = (
         <Flex direction="column" style={{ flex: 1 }} mt="sm">
             {children}
         </Flex>
     );
+
     return (
         <AppShell
+            header={{
+                height: isMobile ? 65 : 0,
+                collapsed: !isMobile || opened,
+            }}
             navbar={{
                 width: SIDE_WIDTH,
                 breakpoint: SIDE_BREAKPOINT,
+                collapsed: { mobile: !opened, desktop: false },
             }}
             styles={{
                 main: {
