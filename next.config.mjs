9721a6e9e0e3b0c6eb6708bbe581935359e8a4b0
Monroe Clinton <contact@monroeclinton.com>
2023-11-15 20:43:24 -0300

    transpilePackages

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,8 @@ await import("./src/env.mjs");
 const config = {
     reactStrictMode: true,
 
+    transpilePackages: ["ol", "rlayers"],
+
     /**
      * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
      * out.
