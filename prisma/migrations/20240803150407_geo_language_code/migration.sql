30ef9bb1350fd111fd28372edf0de1dd64ee56a4
Monroe Clinton <contact@monroeclinton.com>
2024-08-03 11:46:52 -0400

    add country code when translating

--- prisma/migrations/20240803150407_geo_language_code/migration.sql
+++ prisma/migrations/20240803150407_geo_language_code/migration.sql
@@ -1,10 +1,11 @@
 -- CreateTable
-CREATE TABLE "GeoLanguageCode" (
+CREATE TABLE "geo_language_code" (
     "id" TEXT NOT NULL,
-    "code" TEXT NOT NULL,
+    "languageCode" TEXT NOT NULL,
+    "countryCode" TEXT NOT NULL,
     "coordinate" geometry(Point, 4326) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "GeoLanguageCode_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "geo_language_code_pkey" PRIMARY KEY ("id")
 );
