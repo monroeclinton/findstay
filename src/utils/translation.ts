c9e1b50792ccd92b8821bbdf61629b208c24c93f
Monroe Clinton <contact@monroeclinton.com>
2024-08-03 20:15:13 -0400

    Return language from country list

--- src/utils/translation.ts
+++ src/utils/translation.ts
@@ -1,98 +1,33 @@
-import { createId } from "@paralleldrive/cuid2";
-import { Prisma } from "@prisma/client";
 import { iso1A2Code } from "@rapideditor/country-coder";
-import { Ollama } from "ollama";
+import clm from "country-locale-map";
 
-import { prisma } from "~/server/db";
 import { INTERESTS, type InterestType } from "~/types/interests";
 
-const getLanguageCode = async (
+const getLanguageCode = (
     latitude: number,
     longitude: number
-): Promise<string | null> => {
+): string | null => {
     const countryCode = iso1A2Code([longitude, latitude]);
 
     if (!countryCode) {
         return null;
     }
 
-    const result = (
-        await prisma.$queryRaw<[{ languageCode: string }]>(
-            Prisma.sql`
-                SELECT
-                    "languageCode"
-                FROM
-                    geo_language_code
-                WHERE
-                    ST_DistanceSphere(
-                        coordinate,
-                        ST_MakePoint(${longitude}, ${latitude})
-                    ) <= 25000
-                AND
-                    "countryCode" = ${countryCode.toLowerCase()}
-                LIMIT 1
-            `
-        )
-    ).at(0);
+    const country = clm.getCountryByAlpha2(countryCode);
 
-    if (result) {
-        return result.languageCode;
-    }
-
-    const ollama = new Ollama({ host: "http://findstay-ollama:11434" });
-    const response = await ollama.chat({
-        model: "llama3:instruct",
-        messages: [
-            {
-                role: "user",
-                content: `
-                    You are a coordinate to ISO 639-1 tool.
-                    Given the coordinates (${latitude}, ${longitude}) return the ISO 639-1
-                    for the official language at the coordinates and nothing else.
-                `,
-            },
-        ],
-    });
-
-    const languageCode = response.message.content;
-
-    if (languageCode.length !== 2) {
+    if (country?.languages.length !== 1) {
         return null;
     }
 
-    await prisma.$queryRaw<[{ id: string }]>(
-        Prisma.sql`
-                INSERT INTO geo_language_code (
-                    id,
-                    "countryCode",
-                    "languageCode",
-                    coordinate,
-                    "updatedAt",
-                    "createdAt"
-                )
-                VALUES (
-                    ${createId()},
-                    ${countryCode.toLowerCase()},
-                    ${languageCode.toLowerCase()},
-                    ST_POINT(
-                        ${longitude},
-                        ${latitude}
-                    ),
-                    NOW(),
-                    NOW()
-                )
-            `
-    );
-
-    return languageCode.toLowerCase();
+    return country.languages.at(0) as string;
 };
 
-export const getInterestTranslation = async (
+export const getInterestTranslation = (
     query: InterestType,
     latitude: number,
     longitude: number
-): Promise<string> => {
-    const code = await getLanguageCode(latitude, longitude);
+): string => {
+    const code = getLanguageCode(latitude, longitude);
 
     if (!code) {
         return query;
