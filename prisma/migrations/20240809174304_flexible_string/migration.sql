21364864d9a905b472bba4e0baa048b4bb0f1a08
Monroe Clinton <contact@monroeclinton.com>
2024-08-09 17:30:22 -0400

    Change to string

---
+++ prisma/migrations/20240809174304_flexible_string/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "airbnb_location_sync" ALTER COLUMN "flexibleDate" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "stay_sync_params" ALTER COLUMN "flexibleDate" SET DATA TYPE TEXT;
